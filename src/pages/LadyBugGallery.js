ee63bd4e1704f4b3adc5d405f414bd8b8013e2d7
Lesuz <50194165+Lesuz@users.noreply.github.com>
2021-02-01 14:35:00 +0200

    added background image and downsized filesizes

--- src/pages/LadyBugGallery.js
+++ src/pages/LadyBugGallery.js
@@ -4,33 +4,39 @@ import ladybug3 from '../img/ladybug3.jpg';
 import ladybug4 from '../img/ladybug4.jpg';
 import ladybug5 from '../img/ladybug5.jpg';
 import ladybug6 from '../img/ladybug6.jpg';
+import thumbnail1 from '../img/ladybug1-thumbnail.jpg';
+import thumbnail2 from '../img/ladybug2-thumbnail.jpg';
+import thumbnail3 from '../img/ladybug3-thumbnail.jpg';
+import thumbnail4 from '../img/ladybug4-thumbnail.jpg';
+import thumbnail5 from '../img/ladybug5-thumbnail.jpg';
+import thumbnail6 from '../img/ladybug6-thumbnail.jpg';
 
 import ImageGallery from 'react-image-gallery'
 
 const images = [
   {
     original: ladybug1,
-    thumbnail: ladybug1
+    thumbnail: thumbnail1
   },
   {
     original: ladybug2,
-    thumbnail: ladybug2
+    thumbnail: thumbnail2
   },
   {
     original: ladybug3,
-    thumbnail: ladybug3
+    thumbnail: thumbnail3
   },
   {
     original: ladybug4,
-    thumbnail: ladybug4
+    thumbnail: thumbnail4
   },
   {
     original: ladybug5,
-    thumbnail: ladybug5
+    thumbnail: thumbnail5
   },
   {
     original: ladybug6,
-    thumbnail: ladybug6
+    thumbnail: thumbnail6
   }
 ]
 
